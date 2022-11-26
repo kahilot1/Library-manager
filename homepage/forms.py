ced23d206386941ac4ea76092f5e9ac04f435b31
kahil <kahilnaga@gmail.com>
2022-11-26 17:20:25 +0400

    Full build
    First full build lots of bugs
    To Do:
    1) PRIORITY Change the book page, page, imporove look

--- homepage/forms.py
+++ homepage/forms.py
@@ -20,3 +20,20 @@ class add_book(forms.Form):
             'id',
             Submit('submit', 'Submit', css_class='btn-success')
         )
+class checkinoutBook(forms.Form):
+    User = forms.CharField(max_length=100, required=True)
+    def __init__(self, text='Check In', *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper
+        self.helper.form_method = 'GET'
+        if text != 'Check In':
+            self.helper.layout = Layout(
+                'User',
+                Submit('submit', text, css_class="btn_btn-primary_btn-lg_btn-block")
+            )
+        else:
+            self.helper.layout = Layout(
+                Submit('submit', text, css_class="btn_btn-primary_btn-lg_btn-block")
+            )
--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import books
-from .forms import add_book
+from .forms import add_book, checkinoutBook
 from django.db.models import Q
 
 boxes = []
@@ -25,11 +25,17 @@ def addBook(request):
                 summary = form.cleaned_data['summary']
                 author = form.cleaned_data['author']
                 id = form.cleaned_data['id']
+                if ' ' in id:
+                    xyz = list(id)
+                    for y, i in enumerate(xyz):
+                        if i == ' ':
+                            xyz[y] = '_'
+                    id = ''.join(xyz)
                 books.objects.create(title=title, summary=summary, author=author, id=id)
                 print("Book added", title, summary, author, id)
-                return render(request, "siteRoot/addBook.html", {'form':add_book(), 'success':True})
+                return render(request, "siteRoot/addBook.html", {'form':add_book(), 'success':True, 'isPartOf':isPartOfGroup(request.user, 'librarian')})
             else:
-                return render(request, "siteRoot/addBook.html", {'form':form, 'success':False})
+                return render(request, "siteRoot/addBook.html", {'form':form, 'success':False, 'isPartOf':isPartOfGroup(request.user, 'librarian')})
         form = add_book()
         return render(request, 'siteRoot/addBook.html', {'form': form, 'isPartOf':isPartOfGroup(request.user, 'librarian')})
     return render(request, 'siteRoot/home.html' , {'isPartOf':isPartOfGroup(request.user, 'librarian')})
@@ -45,7 +51,36 @@ def listBook(request):
     return render(request, "siteRoot/bookList.html", {"boxes":boxes, 'yes':len(boxes) < 10, 'box':box, 'isPartOf':isPartOfGroup(request.user, 'librarian')})
 
 def bookPage(request, book_id):
-    return render(request, "siteRoot/bookPage.html", {"book_id":books.objects.get(id=book_id), 'isPartOf':isPartOfGroup(request.user, 'librarian')})
+    booky = books.objects.get(id=book_id)
+    if request.method == "GET":
+        print('Getting data and proccessing', request.GET)
+        if booky.checked_out:
+            text = 'Check In'
+        else:
+            text='Check Out'
+        form = checkinoutBook(text, request.GET)
+        print('Data is Valid')
+        if form.is_valid():
+            if text == 'Check Out':
+                text = 'Check In'
+                booky.checked_out = True
+                booky.book_owner = form['User'].value().strip()
+                booky.save()
+                form = checkinoutBook(text)
+        else:
+            text = 'Check Out'
+            booky.checked_out = False
+            booky.book_owner = "Librarian"
+            booky.save()
+            form = checkinoutBook(text)
+
+        
+        return render(request, 'siteRoot/bookPage.html', {'book_id':booky, 'form':form, 'isPartOf':isPartOfGroup(request.user, 'librarian')})
+    if booky.checked_out:
+        form = checkinoutBook()
+    else:
+        form = checkinoutBook('Check Out')
+    return render(request, "siteRoot/bookPage.html", {"book_id":booky, 'isPartOf':isPartOfGroup(request.user, 'librarian'), 'form':form})
 
 def search(request):
     query = None
@@ -55,4 +90,8 @@ def search(request):
         query = request.GET.get("search")
         result = books.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
         result1 = (books.objects.filter(id__icontains=query))
-    return render(request, 'siteRoot/search.html', {'query':query, 'result':result, 'yes':len(result) < 10, 'result1':result1,  'isPartOf':isPartOfGroup(request.user, 'librarian')})
+        result2 = []
+        for xy, i in enumerate(result1):
+            if i.id == query:
+                result2.append(result1[xy])
+    return render(request, 'siteRoot/search.html', {'query':query, 'result':result, 'yes':len(result) < 10, 'result1':result2,  'isPartOf':isPartOfGroup(request.user, 'librarian')})
